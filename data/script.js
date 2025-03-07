c61d3737b59f3051e56bc197d11eda599c8ebaef
sulincix <parduscix@yandex.ru>
2025-03-07 23:21:31 +0000

    touchpad added

--- data/script.js
+++ data/script.js
@@ -46,6 +46,13 @@ tablet.addEventListener('touchstart', on_press);
 tablet.addEventListener('touchmove', on_move);
 tablet.addEventListener('touchend', on_release);
 
+touchpad.addEventListener('touchstart', on_touchpad_start);
+touchpad.addEventListener('touchend', on_touchpad_end);
+touchpad.addEventListener('touchmove', on_touchpad_move);
+touchpad.addEventListener('mousedown', on_touchpad_start);
+touchpad.addEventListener('mouseup', on_touchpad_end);
+touchpad.addEventListener('mousemove', on_touchpad_move);
+
 // Handle key press events
 function on_key_press(code) {
     console.log(code);
@@ -125,19 +132,120 @@ function on_move(e) {
     socket.send(tbody);
 }
 
+var lastPos = { x: 0, y: 0 };
+var lastTapTime = 0;
+var tapTimeout = 300;
+var pressed = false;
+function on_touchpad_start(e) {
+    e.preventDefault();
+    pressed = true;
+    var currentTime = new Date().getTime();
+    var rect = tablet.getBoundingClientRect();
+    var tapEvent = false;
+    if (e.touches) {
+        pos.x = e.touches[0].clientX - rect.left;
+        pos.y = e.touches[0].clientY - rect.top;
+    } else {
+        pos.x = e.clientX - rect.left;
+        pos.y = e.clientY - rect.top;
+    }
+    
+    // Check if the time between taps is within the double-tap threshold
+    if (currentTime - lastTapTime <= tapTimeout) {
+        tapEvent = true;
+    }
+
+    // Update the last tap time
+    lastTapTime = currentTime;
+
+    // Capture initial touch position
+    lastPos.x = pos.x - rect.left;
+    lastPos.y = pos.y - rect.top;
+    
+    
+    if (tapEvent) {
+        var tbody = "type:\tEV_KEY\n";
+        tbody += "code:\tBTN_LEFT\n";
+        tbody += "value:\t1\n\0";
+        socket.send(tbody);
+
+        tbody = "type:\tEV_KEY\n";
+        tbody += "code:\tBTN_LEFT\n";
+        tbody += "value:\t0\n\0";
+        socket.send(tbody);
+    }
+}
+
+function on_touchpad_end(e) {
+    pressed = false;
+}
+
+function on_touchpad_move(e) {
+    e.preventDefault();
+    if(!pressed){
+        return;
+    }
+    var rect = tablet.getBoundingClientRect();
+    if (e.touches) {
+        pos.x = e.touches[0].clientX - rect.left;
+        pos.y = e.touches[0].clientY - rect.top;
+    } else {
+        pos.x = e.clientX - rect.left;
+        pos.y = e.clientY - rect.top;
+    }
+    var currentPos = {
+        x: pos.x - rect.left,
+        y: pos.y - rect.top
+    };
+
+    // Calculate the relative movement
+    var deltaX = currentPos.x - lastPos.x;
+    var deltaY = currentPos.y - lastPos.y;
+
+    // Send relative movement
+    send_relative_movement(deltaX, deltaY);
+
+    // Update last position
+    lastPos = currentPos;
+}
+
+function send_relative_movement(deltaX, deltaY) {
+    // Send relative X movement
+    var tbody = "type:\tEV_REL\n";
+    tbody += "code:\tREL_X\n";
+    tbody += "value:\t" + deltaX + "\n\0";
+    socket.send(tbody);
+
+    // Send relative Y movement
+    tbody = "type:\tEV_REL\n";
+    tbody += "code:\tREL_Y\n";
+    tbody += "value:\t" + deltaY + "\n\0";
+    socket.send(tbody);
+}
+
 document.getElementById('keyboard').style.display = 'none';
+document.getElementById('touchpad').style.display = 'none';
 let content = document.getElementById('tablet');
 
 function showKeyboard() {
     document.getElementById('keyboard').style.display = 'block';
     document.getElementById('tablet').style.display = 'none';
+    document.getElementById('touchpad').style.display = 'none';
     content = document.getElementById('keyboard');
 }
 
 function showGfxTablet() {
     document.getElementById('keyboard').style.display = 'none';
     document.getElementById('tablet').style.display = 'block';
+    document.getElementById('touchpad').style.display = 'none';
     content = document.getElementById('tablet');
+}
+
+function showTouchpad() {
+    document.getElementById('keyboard').style.display = 'none';
+    document.getElementById('tablet').style.display = 'none';
+    document.getElementById('touchpad').style.display = 'block';
+    content = document.getElementById('touchpad');
 }
 
 function showFullScreen() {
@@ -160,9 +268,16 @@ const keyboardLayoutLabels = [
     ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', '\''],
     ['Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', 'Enter'],
     ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'Up'],
-    ['Ctrl', 'Alt', 'Space', 'Altgr', 'Ctrl', 'Left', 'Down', 'Right']
+    ['Ctrl', 'Alt', 'Space', 'Altgr', 'Left', 'Down', 'Right']
 ];
 
+// Object to keep track of the toggle states of modifier keys
+const modifierStates = {
+    'Ctrl': false,
+    'Alt': false,
+    'Altgr': false,
+    'Shift': false
+};
 
 // Function to create the keyboard
 function createKeyboard() {
@@ -178,8 +293,30 @@ function createKeyboard() {
             keyButton.textContent = key;
 
             // Add mouse event listeners
-            keyButton.onmousedown = (e) => on_key_press(getValueFromLabel(key));
-            keyButton.onmouseup = (e) => on_key_release(getValueFromLabel(key));
+            keyButton.onmousedown = (e) => {
+                if (modifierStates.hasOwnProperty(key)) {
+                    // Toggle the state of the modifier key
+                    if (!modifierStates[key]) {
+                        // If the key is not active, send press event
+                        on_key_press(getValueFromLabel(key));
+                        keyButton.classList.add('active');
+                    } else {
+                        // If the key is already active, send release event
+                        on_key_release(getValueFromLabel(key));
+                        keyButton.classList.remove('active');
+                    }
+                    // Toggle the state
+                    modifierStates[key] = !modifierStates[key];
+                } else {
+                    on_key_press(getValueFromLabel(key));
+                }
+            };
+
+            keyButton.onmouseup = (e) => {
+                if (!modifierStates.hasOwnProperty(key)) {
+                    on_key_release(getValueFromLabel(key));
+                }
+            };
 
             rowDiv.appendChild(keyButton);
         });
