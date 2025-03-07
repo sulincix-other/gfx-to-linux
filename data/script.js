844ff000e42ebb8fca2092ef70b93c5e5f03baf8
sulincix <parduscix@yandex.ru>
2025-03-07 22:46:26 +0000

    add more keys

--- data/script.js
+++ data/script.js
@@ -156,12 +156,13 @@ function showFullScreen() {
 
 // Define the keyboard layout
 const keyboardLayoutLabels = [
-    ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='],
-    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']'],
-    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\''],
-    ['Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/'],
-    [' ', 'Enter', 'Backspace']
+    ['Esc', '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+    ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', '\''],
+    ['Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', 'Enter'],
+    ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'Up'],
+    ['Ctrl', 'Alt', 'Space', 'Altgr', 'Ctrl', 'Left', 'Down', 'Right']
 ];
+
 
 // Function to create the keyboard
 function createKeyboard() {
